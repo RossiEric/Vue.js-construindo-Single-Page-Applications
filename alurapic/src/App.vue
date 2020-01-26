8a9e4350b294e0114b8b1c8ada26ea3fd711562c
Eric Rossi <rossieric@hotmail.com>
2020-01-26 14:06:01 -0300

    consulta API com VueResource

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -22,18 +22,20 @@ export default {
   data() {
     return {
       titulo: 'Alurapic',
-      fotos: [
-        {
-          url: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTwV4kVzT5McBdGSgqlVeRzubrNH_mOrrkKseDOGFURq20HmsrelEfMU7It',
-          titulo: 'Cachorro'
-        },
-        {
-          url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOhmlmzV4-Sifx5BIc2SXeA-1CtZJf8jb8V_vPZyKbXIQJKU-rkxGO6OM',
-          titulo: 'Gato'
-        }
-      ]
-
+      fotos: []
     }
+  },
+  //Options / Lifecycle Hooks
+  //executa quando o componente é criado, antes de data()
+  created(){
+    console.log('componente criado');
+
+    //consulta API com VueResource
+    this.$http.get('http://localhost:3000/v1/fotos')
+      .then(res => res.json())
+      //this.fotos definida em data()
+      .then(fotos => this.fotos = fotos, err => console.log(err));
+
   }
 }
 </script>
--- alurapic/src/main.js
+++ alurapic/src/main.js
@@ -4,6 +4,15 @@ import Vue from 'vue'
 //componente
 import App from './App.vue'
 
+/*
+O módulo VueResource, baixado através do npm, é aquele que disponibiliza artefatos especializados na realização de requisições assíncronas ao mesmo tempo em que se integra com o ecossistema do Vue
+*/
+//importando modulos
+import VueResource from 'vue-resource';
+
+//use VueResource do import modulo
+Vue.use(VueResource)
+
 //criando uma view instance
 new Vue({
   el: '#app', //onde quero renderizar o componente
